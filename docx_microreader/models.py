9251ca71c79c5af247a6793c920b63635ea713b4
DudaEugen <54473424+DudaEugen@users.noreply.github.com>
2020-03-05 12:41:42 +0200

    tag_name is protected now + rename get_tag and gat_tags methods to get_element and get_elements

--- docx_microreader/models.py
+++ docx_microreader/models.py
@@ -1,38 +1,39 @@
 import zipfile
 import xml.dom.minidom
 import re
-from typing import List, Tuple
+from typing import List, Tuple, ClassVar
 from .special_characters import *
 from .support_classes import ContentInf
 
 
 class XMLement:
     _output_format: str = 'html'
-    tag_name: str
+    _tag_name: str
 
     def __init__(self, element: ContentInf):
         self._content: str = element.content
         self._begin: int = element.begin
         self._end: int = element.end
 
-    def _get_tag(self, tag: str = '') -> ContentInf:
-        _tag = self.tag_name if tag == '' else tag
-        element = re.search(rf'<{_tag}( [^\n>%]+)?>([^%]+)?</{_tag}>', self._content)
+    def _get_element(self, element_class) -> ContentInf:
+        tag: XMLement = element_class._tag_name
+        element = re.search(rf'<{tag}( [^\n>%]+)?>([^%]+)?</{tag}>', self._content)
         return ContentInf(element.group(0), element.span())
 
-    def _get_tags(self, tag) -> List[ContentInf]:
-        tags: List[ContentInf] = []
+    def _get_elements(self, element_class) -> List[ContentInf]:
+        tag: XMLement = element_class._tag_name
+        elements: List[ContentInf] = []
         for o in re.finditer(rf'<{tag}( [^\n>%]+)?>[^%]+?</{tag}>', self._content):
-            tags.append(ContentInf(o.group(0), o.span()))
-        return tags
+            elements.append(ContentInf(o.group(0), o.span()))
+        return elements
 
     def _inner_content(self) -> str:
-        inner_content = re.sub(rf'<{self.tag_name}([^\n>%]+)?>', '', self._content)
-        inner_content = re.sub(rf'</{self.tag_name}>', '', inner_content)
+        inner_content = re.sub(rf'<{self._tag_name}([^\n>%]+)?>', '', self._content)
+        inner_content = re.sub(rf'</{self._tag_name}>', '', inner_content)
         return inner_content
 
     def _get_properties(self, tag_name: str) -> str or None:
-        properties = re.search(rf'<{self.tag_name}Pr>([^%]+)?</{self.tag_name}Pr>', self._content).group(0)
+        properties = re.search(rf'<{self._tag_name}Pr>([^%]+)?</{self._tag_name}Pr>', self._content).group(0)
         prop = re.search(rf'<{tag_name} w:val="([^"%]+)"/>', properties)
         if prop:
             p = prop.group(0)
@@ -41,12 +42,12 @@ class XMLement:
             return p[begin: end]
 
     def _have_properties(self, tag_name: str) -> bool:
-        properties = re.search(rf'<{self.tag_name}([^\n>%]+)?>([^%]+)?</{self.tag_name}>', self._content).group(0)
+        properties = re.search(rf'<{self._tag_name}([^\n>%]+)?>([^%]+)?</{self._tag_name}>', self._content).group(0)
         return True if (properties.find(rf'<{tag_name}/>') != -1) else False
 
 
 class Text(XMLement):
-    tag_name: str = 'w:t'
+    _tag_name: str = 'w:t'
 
     def __init__(self, element: ContentInf):
         super(Text, self).__init__(element)
@@ -57,11 +58,11 @@ class Text(XMLement):
 
 
 class Run(XMLement):
-    tag_name: str = 'w:r'
+    _tag_name: str = 'w:r'
 
     def __init__(self,  element: ContentInf):
         super(Run, self).__init__(element)
-        text_tuple = self._get_tag(Text.tag_name)
+        text_tuple = self._get_element(Text)
         self.text: Text = Text(text_tuple)
         self._is_bold: bool = self._have_properties('w:b')
         self._is_italic: bool = self._have_properties('w:i')
@@ -93,12 +94,12 @@ class Run(XMLement):
 
 
 class Paragraph(XMLement):
-    tag_name: str = 'w:p'
+    _tag_name: str = 'w:p'
 
     def __init__(self, element: ContentInf):
         super(Paragraph, self).__init__(element)
         self.runs: List[Run] = []
-        run_tuples = self._get_tags(Run.tag_name)
+        run_tuples = self._get_elements(Run)
         for r in run_tuples:
             run = Run(r)
             self.runs.append(run)
@@ -113,16 +114,16 @@ class Paragraph(XMLement):
 
 
 class Document(XMLement):
-    tag_name: str = 'w:body'
+    _tag_name: str = 'w:body'
 
     def __init__(self, path: str):
         self._content: str = xml.dom.minidom.parseString(
             zipfile.ZipFile(path).read('word/document.xml')
         ).toprettyxml()
-        doc: ContentInf = self._get_tag(Document.tag_name)
+        doc: ContentInf = self._get_element(Document)
         super(Document, self).__init__(doc)
         self.paragraphs: List[Paragraph] = []
-        paragraph_tuples = self._get_tags(Paragraph.tag_name)
+        paragraph_tuples = self._get_elements(Paragraph)
         for p in paragraph_tuples:
             par = Paragraph(p)
             self.paragraphs.append(par)
