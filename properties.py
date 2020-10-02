7de8df3b37ff6f4eb2fb2043f0d4368a61cd8364
DudaEugen <54473424+DudaEugen@users.noreply.github.com>
2020-10-02 14:49:11 +0300

    tag_wrap of PropertyDescription can be List[str] now

--- properties.py
+++ properties.py
@@ -3,20 +3,30 @@ from typing import Union, List
 
 class PropertyDescription:
 
-    def __init__(self, tag_wrap: Union[str, None], tag: Union[List[str], str],
+    def __init__(self, tag_wrap: Union[List[str], str, None], tag: Union[List[str], str],
                  tag_property: Union[List[str], str, None]):
-        self.tag_wrap: Union[str, None] = tag_wrap
+        self.tag_wrap: Union[List[str], str, None] = tag_wrap
         self.tag: Union[List[str], str] = tag
         self.tag_property: Union[List[str], str, None] = tag_property
         self.value_type: str = 'str' if self.tag_property is not None else 'bool'
 
     def get_wrapped_tags(self) -> Union[List[str], str]:
+        tag_wrap: Union[List[str], str] = self.__wrap_for_tag()
+        if isinstance(self.tag, list) and isinstance(tag_wrap, list):
+            result: List[str] = []
+            for wrap in tag_wrap:
+                result.extend([(wrap + tag) for tag in self.tag])
+            return result
         if isinstance(self.tag, list):
-            return [self.__wrap_for_tag() + tag for tag in self.tag]
-        return self.__wrap_for_tag() + self.tag
+            return [(tag_wrap + tag) for tag in self.tag]
+        if isinstance(tag_wrap, list):
+            return [(wrap + self.tag) for wrap in tag_wrap]
+        return tag_wrap + self.tag
 
-    def __wrap_for_tag(self) -> str:
+    def __wrap_for_tag(self) -> Union[List[str], str]:
         if self.tag_wrap is not None:
+            if isinstance(self.tag_wrap, list):
+                return [(tag_wrap + '/') for tag_wrap in self.tag_wrap]
             return self.tag_wrap + '/'
         return ''
 
